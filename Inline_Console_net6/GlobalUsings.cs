b478b07860bc41c45435033f52df582c48b13d72
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-01 12:33:59 +0300

    Add global usings.

--- Inline_Console_net6/GlobalUsings.cs
+++ Inline_Console_net6/GlobalUsings.cs
@@ -1,1 +1,7 @@
-﻿global using static Inline_Console_net6.Namespace1.StaticExtensionMethodDeclaration;
+﻿// GlobalUsings.cs
+// 2022 02 01 12:32 PM
+
+global using static Inline_Console_net6.Namespace1.StaticExtensionMethodDeclaration;
+//global using NotExisting;
+global using Inline_Referenced_Lib;
+// TODO: type global using Alias =
