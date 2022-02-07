df3eac985b6ef2d05134b6a5d4856228d3e36f0a
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-07 15:38:17 +0300

    Add nuget reference.

---
+++ Inline_Console_net6/UseNuget.cs
@@ -0,0 +1,14 @@
+﻿// UseNuget.cs
+// 2022 02 07 3:37 PM
+
+using System.Text.Json.Nodes;
+
+namespace Inline_Core_Console_net6;
+
+public class UseNuget
+{
+    private void Test()
+    {
+        new JsonArray();
+    }
+}
