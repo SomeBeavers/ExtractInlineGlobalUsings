99e51c1834f0a9277d8082f0a3258584006e545d
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-07 12:58:51 +0300

    Add folder.

---
+++ Extract_Console_net6/GlobalUsings.generated.cs
@@ -0,0 +1,9 @@
+﻿// GlobalUsings.generated.cs
+// 2022 02 07 12:55 PM
+
+namespace Extract_Core_Console_net6;
+
+public class GlobalUsings_generated
+{
+    
+}
---
+++ Extract_Console_net6/GlobalUsingsAreHere/ClassInFolder.cs
@@ -0,0 +1,14 @@
+﻿// ClassInFolder.cs
+// 2022 02 07 12:56 PM
+
+using System.Collections.Generic;
+
+namespace Extract_Core_Console_net6.GlobalUsingsAreHere;
+
+public class ClassInFolder
+{
+    private void Test()
+    {
+        new List<string>();
+    }
+}
