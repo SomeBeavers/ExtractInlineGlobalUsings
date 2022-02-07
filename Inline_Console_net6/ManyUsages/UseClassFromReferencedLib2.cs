001c74bd9a190445283c2e4ff7fab90f2014297b
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-07 11:28:11 +0300

    Remove unnecessary usings.

--- Inline_Console_net6/ManyUsages/UseClassFromReferencedLib2.cs
+++ Inline_Console_net6/ManyUsages/UseClassFromReferencedLib2.cs
@@ -1,8 +1,6 @@
 ﻿// UseClassFromReferencedLib2.cs
 // 2022 02 07 11:08 AM
 
-using Inline_Referenced_Lib;
-
 namespace Inline_Core_Console_net6;
 
 public class UseClassFromReferencedLib2
--- Inline_Console_net6/ManyUsages/UseClassFromReferencedLib4.cs
+++ Inline_Console_net6/ManyUsages/UseClassFromReferencedLib4.cs
@@ -1,8 +1,6 @@
 ﻿// UseClassFromReferencedLib4.cs
 // 2022 02 07 11:09 AM
 
-using Inline_Referenced_Lib;
-
 namespace Inline_Core_Console_net6;
 
 public class UseClassFromReferencedLib4
