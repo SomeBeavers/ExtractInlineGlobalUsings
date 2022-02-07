69cf47a5b262ddf7eccc2577aaa0d0dbf1ac4cf1
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-07 12:06:05 +0300

    Add global using to MVC.

---
+++ Inline_MVC/Global.generated.cs
@@ -0,0 +1,1 @@
+﻿global using Inline_MVC.Models1;
---
+++ Inline_MVC/Models/UseErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿// UseErrorViewModel.cs
+// 2022 02 07 12:05 PM
+
+namespace Inline_MVC.Models1;
+
+public class UseErrorViewModel
+{
+    private void Test()
+    {
+        new ErrorViewModel();
+    }
+}
