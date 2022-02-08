1782677385ad77c6728af90134d326ccb5ce20f7
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-08 17:15:56 +0300

    Set IDE0005 as warning.

--- Extract_Console_net6/GlobalUsings.cs
+++ Extract_Console_net6/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿//comment
-// ReSharper disable once RedundantUsingDirective
-using System.Security.AccessControl;
+﻿#pragma warning disable IDE0005 // Using directive is unnecessary.
+// ReSharper disable once RedundantUsingDirective.Global
+global using System.Security.AccessControl;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
