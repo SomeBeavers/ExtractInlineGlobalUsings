c2ece023334c8f10e1ab713f7e5be1e4674c41c1
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-08 11:23:20 +0300

    Add Shared1.cs

---
+++ Shared/Shared1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    internal class Shared1
+    {
+        public string SharedName { get; set; }
+    }
+}
