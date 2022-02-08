ff91d6649a5cf3050fd56ab17d66e8ad2a4f92a8
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-08 12:39:27 +0300

    Add copy.

---
+++ Extract_Console_net6/Static/StaticExtensionMethodUsage2.cs
@@ -0,0 +1,17 @@
+﻿// StaticExtensionMethodUsage2.cs
+// 2022 02 08 12:39 PM
+
+using System;
+
+using Inline_Console_net6.Namespace1;
+
+namespace Inline_Console_net6.Namespace2;
+
+public class StaticExtensionMethodUsage2
+{
+    public void Test(int num)
+    {
+        var test = num.TestExtensionMethod();
+        Console.WriteLine();
+    }
+}
