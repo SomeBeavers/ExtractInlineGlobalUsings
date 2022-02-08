91aefd0de43941fd48eb98027c591fd44eec9847
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-08 12:29:22 +0300

    Add simple using to extract core.

---
+++ Extract_Console_net6/SimpleUsing.cs
@@ -0,0 +1,24 @@
+﻿// SimpleUsing.cs
+// 2022 02 08 12:26 PM
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AnotherNamespace;
+
+using Serilog;
+
+namespace Extract_Core_Console_net6;
+
+public class SimpleUsing
+{
+    private ILogger logger;
+
+    private void Test(UseMe parameter, List<UseMe> list)
+    {
+        Console.WriteLine(list.Any());
+
+        logger.Error("{testMessage}", parameter);
+    }
+}
---
+++ Extract_Console_net6/UseMe.cs
@@ -0,0 +1,8 @@
+﻿// UseMe.cs
+// 2022 02 08 12:28 PM
+
+namespace AnotherNamespace;
+
+internal class UseMe
+{
+}
