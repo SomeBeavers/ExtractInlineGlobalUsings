c5fd3b17070674d645c9bf82b68fb8c6204a98c6
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-08 11:26:45 +0300

    Add usages in Shared.

--- Shared/Shared1.cs
+++ Shared/Shared1.cs
@@ -7,5 +7,15 @@ namespace Shared
     internal class Shared1
     {
         public string SharedName { get; set; }
+
+        private void Test()
+        {
+            SharedName = "";
+            Console.WriteLine();
+            foreach (var s in new List<string>())
+            {
+                Console.WriteLine(s);
+            }
+        }
     }
 }
