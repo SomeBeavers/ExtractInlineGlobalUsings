e69a56379813a93caa97f2d0ce52447989d31440
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-10 11:20:21 +0300

    Add usings in Blazor.

---
+++ Extract_Blazor/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Class1.cs
+// 2022 02 10 11:18 AM
+
+namespace Extract_Blazor.Code;
+
+public class Class1
+{
+    public string Name { get; set; }
+}
---
+++ Extract_Blazor/Class2.cs
@@ -0,0 +1,14 @@
+﻿// Class2.cs
+// 2022 02 10 11:19 AM
+
+using Extract_Blazor.Code;
+
+namespace Extract_Blazor;
+
+public class Class2
+{
+    private void Test()
+    {
+        Console.WriteLine(new Class1().Name);
+    }
+}
