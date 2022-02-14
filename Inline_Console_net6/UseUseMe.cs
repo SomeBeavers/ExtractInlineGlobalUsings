c44df3410d891782f2160e723f17a8bf43c57098
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-02-14 10:54:58 +0300

    Update.

--- Inline_Console_net6/UseUseMe.cs
+++ Inline_Console_net6/UseUseMe.cs
@@ -3,7 +3,7 @@
 
 
 namespace Inline_Core_Console_net6;
-
+using Alias = Inline_Core_Console_net6.Use3;
 public class UseUseMe
 {
     private void Test()
@@ -11,5 +11,12 @@ public class UseUseMe
         // ReSharper disable once UnusedVariable
         var useMe = new Alias.UseMe();
         var vector2 = One;
+    }
+}
+
+public static class Use3
+{
+    public class UseMe
+    {
     }
 }
